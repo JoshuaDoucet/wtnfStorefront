51f686717bfe5fa3a6ec8062bf60626f1dc3e980
Joshua Doucet <joshua.doucet@live.com>
2022-02-23 15:05:53 -0700

    configure FE to interact with API on aws

--- wtnfStorefrontFrontEnd/src/app/services/configuration/configuration.service.ts
+++ wtnfStorefrontFrontEnd/src/app/services/configuration/configuration.service.ts
@@ -4,7 +4,8 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ConfigurationService {
-  private apiHost: string = 'http://52.0.169.67:3000';
+  private apiHost: string = 'http://wtnfapi-env.eba-uzamsy27.us-east-1.elasticbeanstalk.com:3000';
+        // 'http://52.0.169.67:3000';
 
   constructor() {}
 
