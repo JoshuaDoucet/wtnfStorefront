70100547867e267cd24cff1f55c544036a3380a2
Joshua Doucet <joshua.doucet@live.com>
2022-02-25 15:39:05 -0700

    eb use fix

--- wtnfStorefrontBackEnd/bin/deploy.sh
+++ wtnfStorefrontBackEnd/bin/deploy.sh
@@ -1,5 +1,5 @@
 eb init wtnf-api --region $AWS_DEFAULT_REGION
-eb use wtnf-api --profile default
+eb use Wtnfapi-env --profile default
 eb setenv AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION  --profile default
 eb deploy --profile default 
 
