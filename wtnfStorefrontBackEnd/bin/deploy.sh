361f242c4438f6b498100aeacd39dfb40ffa1a26
Joshua Doucet <joshua.doucet@live.com>
2022-02-25 15:36:19 -0700

    add new circleCI config

--- wtnfStorefrontBackEnd/bin/deploy.sh
+++ wtnfStorefrontBackEnd/bin/deploy.sh
@@ -1,5 +1,5 @@
-eb init Wtnfapi-env --platform node.js --region $AWS_DEFAULT_REGION
-eb use Wtnfapi-env --profile default
+eb init wtnf-api --region $AWS_DEFAULT_REGION
+eb use wtnf-api --profile default
 eb setenv AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION  --profile default
 eb deploy --profile default 
 
